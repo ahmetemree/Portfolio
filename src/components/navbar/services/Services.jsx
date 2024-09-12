b1b6cfce72d5df97044089e97068aa97097c02dc
Ahmet Emre <127850172+ahmetemree@users.noreply.github.com>
2024-09-12 14:57:06 +0300

    fix

--- src/components/navbar/services/Services.jsx
+++ src/components/navbar/services/Services.jsx
@@ -1,16 +1,44 @@
+import { useRef } from "react";
 import "./services.scss";
-import { motion } from "framer-motion";
+import { animate, motion,useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 const Services = () => {
+
+    const ref = useRef()
+     const isInView = useInView(ref,{margin:"-100px"})
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+    //   whileInView="animate"
+    ref={ref}
+    animate={isInView && "animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
         <p>
           I can help your business grow up
           <br /> and move forward
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <img src="/people.webp" alt="" />
           <h1>
@@ -24,32 +52,49 @@ const Services = () => {
           <button>What Can I Do?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
-        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
           <h2>Experience</h2>
           <p>
-            I can create your software with all my experiences gained from past projects which I developed.
+            I can create your software with all my experiences gained from past
+            projects which I developed.
           </p>
           <button>Go</button>
         </motion.div>
-        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
           <h2>User-Friendly Softwares</h2>
           <p>
-          I care about user experience while using the product. User experience in my projects always has priority!
+            I care about user experience while using the product. User
+            experience in my projects always has priority!
           </p>
           <button>Go</button>
         </motion.div>
-        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
           <h2>Secure</h2>
           <p>
-            I consider the security issue with all my software products. Your data and your privacy always under protection!
+            I consider the security issue with all my software products. Your
+            data and your privacy always under protection!
           </p>
           <button>Go</button>
         </motion.div>
-        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
           <h2>Faster Products</h2>
           <p>
-            As a Software Engineer, I can say that the software product should have fast interface and smooth transitions. Your software will definitely all of these!
+            As a Software Engineer, I can say that the software product should
+            have fast interface and smooth transitions. Your software will
+            definitely all of these!
           </p>
           <button>Go</button>
         </motion.div>

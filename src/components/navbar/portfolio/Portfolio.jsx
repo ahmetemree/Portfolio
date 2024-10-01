73ea2d89b90be208ca51c6e8910f76a172a1b838
Ahmet Emre <127850172+ahmetemree@users.noreply.github.com>
2024-10-01 14:04:49 +0300

    projects added

--- src/components/navbar/portfolio/Portfolio.jsx
+++ src/components/navbar/portfolio/Portfolio.jsx
@@ -4,28 +4,42 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
+    title: "Turbo Tech Oto Aksesuar",
+    img: "./turbotech.png",
+    desc: "A car accessories shopping brand which you can get your needs from there!",
+    website: "https://turbotechotoaksesuar.com/",
+  },
+  {
+    id: 2,
+    title: "AI-Strolog",
+    img: "./aistrolog.png",
+    desc: "An AI supported astrology assistant application which specified only for you.",
+    website: "https://ai-strolog.com/",
+  },
+  {
+    id: 3,
     title: "SmartAI",
-    img: "./smartAI.png",
+    img: "./smartailogo.png",
     desc: "An AI application which you can chat about anything! You can send images, save chats and also It's easy to use!",
     website: "https://smart-ai-two.vercel.app/",
   },
   {
-    id: 2,
+    id: 4,
     title: "Booking-App",
-    img: "./bookingapp.png",
+    img: "./bookinglogo.png",
     desc: "Booking application which provides you to search any estate for stay. You can filter places, save them and send message to their owner!",
     website: "https://booking-app-three-phi.vercel.app/",
   },
   {
-    id: 3,
+    id: 5,
     title: "Chat-App",
-    img: "./chatapp.png",
+    img: "./chatapplogo.png",
     desc: "Chat application for using chatting between saved users. You can register and you can enjoy chatting with other people!",
     website: "https://chatss-app.vercel.app/",
   },
 
   {
-    id: 4,
+    id: 6,
     title: "Pethomealpi",
     img: "https://res.cloudinary.com/dytlryqvs/image/upload/v1723987281/logo_gjafqg.png",
     desc: "An application for you to shopping for your pets&cats. You can buy your needs for your animals in Pethomealpi website (Under production).",
